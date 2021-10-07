19df35b628f6df4046faf5b6eca6ce5f0fdd9254
evyatar <evyatarluv@gmail.com>
2021-10-07 16:40:18 +0300

    temp commit

--- unitree_ros_to_real/unitree_legged_msgs/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/evyatar/catkin_ws/src/unitree_ros_to_real-3.2.1/unitree_legged_msgs/build/devel/include".split(';') if "/home/evyatar/catkin_ws/src/unitree_ros_to_real-3.2.1/unitree_legged_msgs/build/devel/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "message_runtime;std_msgs;geometry_msgs;sensor_msgs".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "unitree_legged_msgs"
-PROJECT_SPACE_DIR = "/home/evyatar/catkin_ws/src/unitree_ros_to_real-3.2.1/unitree_legged_msgs/build/devel"
-PROJECT_VERSION = "0.0.0"
--- unitree_ros_to_real/unitree_legged_msgs/build/gtest/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/evyatar/catkin_ws/src/unitree_ros_to_real-3.2.1/unitree_legged_msgs/build/gtest/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
